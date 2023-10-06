e4768c992cd02a597285ca72125c1a5dcd828c5b
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2023-10-06 13:34:54 -0300

    Adding the BackEnd Folders

---
+++ BackEnd/PHP/Ex 01/formulario.php
@@ -0,0 +1,45 @@
+<><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .formulario{
+            border: solid red 1px;
+            width: 600px;
+            margin: 0;
+            padding: 0;
+        }
+        input{
+
+        }
+    </style>
+</head>
+<body>
+    <div class="formulario">
+        <form method="POST" action="../4.php">&nbsp;&nbsp;
+            <label>Username</label>
+            <input type="text" name="username" id="username" placeholder="username">
+            <br>
+            <label>Password</label>
+            <input type="password" name="password" id="password" placeholder="Password Here">
+            <br>
+            <label>Name</label>
+            <input type="text" name="name" id="name" placeholder="Your Name Here">
+            <br>
+            <label>E-mail</label>
+            <input type="email" name="email" id="email" placeholder="example@email.com">
+            <br>
+            <label>City</label>
+            <input type="text" name="city" id="city" placeholder="City Name Here">
+            <br>
+            <label>Submit Form</label>
+            <input type="submit" value="SUBMIT" name="submit" id="submit">
+            <br>
+        </form>
+    </div>
+</body>
+</html>
---
+++ BackEnd/PHP/validateForm.php
@@ -0,0 +1,84 @@
+<?php
+if($_REQUEST["action"] == "save")
+{
+    $validForm = true;
+    $name_size = strlen($_POST["name"]);
+
+    if($name_size < 5 || $name_size > 64)
+    {
+        echo("The field 'NAME' must have between 5 and 64 characters.".$name_size);
+        $validForm = false;
+    }
+    $age = (int)$_POST["age"];
+    if(is_nan($age) || $age < 4 || $age > 120)
+    {
+        echo("The field 'AGE' must be filled correctly!");
+        $validForm = false;
+    }
+    $email = $_POST["email"];
+    $regex = "/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/";
+    if(!preg_match($regex, $email))
+    {
+        echo("The field email must be filled correctly");
+        $validForm = false;
+    }
+    $gender = $_POST["gender"];
+    if($gender != "M" && $gender != "F")
+    {
+        echo("The field 'gender' must be filled");
+        $validForm = false;
+    }
+    $course = $_POST["course"];
+    if ($course == "" || $course == "Select ...");
+    {
+        echo("The filed 'course' must be filled");
+        $validForm = false;
+    }
+    if ($validForm)
+    {
+        echo("Form Successfully Validated");
+        exit();
+    }
+}
+?>
+<><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Forms Example</title>
+    <script language="JavaScript">
+        function validateForm()
+        {
+            var name_size = document.forms["signup3_3.php"].name.value.lenght;
+            if(name_size < 5 || name_size > 64)
+            {
+                alert("The field name must have between 5 and 64 characters.");
+                return false;
+            }
+
+            var age = document.forms["signup3_3.php"].age.value;
+            if(isNaN(age) || age < 4 || age > 120)
+            {
+                alert("The field age must be filled correctly.");
+                return false;
+            }
+            var email = document.forms["signup3_3.php"].email.value;
+            if(email.length < 5 || email.length > 128 || email.indexOf('@') === -1 || email.indexOf('.') === -1){
+                alert("The email field must be filled correctly!");
+                return false;
+            }
+
+            var gender = document.forms["signup3_3.php"].gender;
+            var sex = false;
+            for();
+
+        }
+    </script>
+</head>
+<body>
+
+</body>
+</html>
